f0cf369a061d4cc6ddf3f893b9617932535f0eff
Ismael Mercado <ismaelgmercado@yahoo.com>
2020-11-11 18:49:16 -0500

    made some changes

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-var connection = mysql.createConnection({
-  host: "mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-  port: 3306,
-  user: "ws6niq7b5bjmh3bn",
-  password: "k0xq7eok1fhuszqq",
-  database: "n74626x9s90z845o"
+ connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Bravo6231",
+  database: "burgers_db"
 });
 }
 
